826dc8a9f7b285a94e015bec5c0240ce21487afd
Anurag <anuragtiw112@gmail.com>
2023-02-08 16:25:31 +0530

    name changed

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json()); // to use req.body
 
 // Mine
-const connectToMongoDB = require("./databases/MongoDB/config");
+const connectToMongoDB = require("./databases/mongoDB/config");
 connectToMongoDB();
 
 const { createDir } = require("./utilities/helper_functions");
